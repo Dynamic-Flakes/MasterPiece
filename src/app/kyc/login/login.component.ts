a41adc8ffed6b3456979fa23d8aff2f0f31dc9d7
Dynamic-Flakes <dynamicflakeslimited@gmail.com>
2018-08-14 22:52:11 +0100

    KyC complete

--- src/app/kyc/login/login.component.ts
+++ src/app/kyc/login/login.component.ts
@@ -55,20 +55,19 @@ export class LoginComponent implements OnInit {
             // CHECK WHERE USER STOPPED IN THE REGISTERATION PROCESS AND CONTINUE
             console.log(`This is the current userMode:- ${res.data.user.userMode}`);
             const _userMode = res.data.user.userMode;
-            // if (_userMode === 'New') {
-            //   // Navigating to Reset Password Page
-            // this.router.navigate(['/kyc/change-password']);
-            // } if (_userMode === 'OTPverify') {
-            //   // Navigating to First Otp Page
-            // this.router.navigate(['/kyc/otp']);
-            // } if (_userMode === 'TransPin') {
-            //   // Navigating to Set Transaction Pin Page
-            // this.router.navigate(['/kyc/transaction-pin']);
-            // } if (_userMode === 'Confirm') {
-            //   // Navigating to Cooperator Dashboard
-            // this.router.navigate(['/shop/history/transactions']);
-            // }
+            if (_userMode === 'New') {
+              // Navigating to Reset Password Page
             this.router.navigate(['/kyc/change-password']);
+            } if (_userMode === 'OTPverify') {
+              // Navigating to First Otp Page
+            this.router.navigate(['/kyc/otp']);
+            } if (_userMode === 'TransPin') {
+              // Navigating to Set Transaction Pin Page
+            this.router.navigate(['/kyc/transaction-pin']);
+            } if (_userMode === 'Confirm') {
+              // Navigating to Cooperator Dashboard
+            this.router.navigate(['/shop/history/transactions']);
+            }
         }
 
         // IF USER IS A VENDOR
@@ -82,20 +81,19 @@ export class LoginComponent implements OnInit {
             // CHECK WHERE USER STOPPED IN THE REGISTERATION PROCESS AND CONTINUE
             console.log(`This is the current userMode:- ${res.data.user.userMode}`);
             const _userMode = res.data.user.userMode;
-            // if (_userMode === 'New') {
-            //   // Navigating to Reset Password Page
-            // this.router.navigate(['/kyc/change-password']);
-            // } if (_userMode === 'OTPverify') {
-            //   // Navigating to First Otp Page
-            // this.router.navigate(['/kyc/otp']);
-            // } if (_userMode === 'AccountDetails') {
-            //   // Navigating to Set Transaction Pin Page
-            // this.router.navigate(['vendor-bank']);
-            // } if (_userMode === 'Confirm') {
-            //   // Navigating to Cooperator Dashboard
-            // this.router.navigate(['/shop/history/transactions']);
-            // }
-             this.router.navigate(['/kyc/change-password']);
+            if (_userMode === 'New') {
+              // Navigating to Reset Password Page
+            this.router.navigate(['/kyc/change-password']);
+            } if (_userMode === 'OTPverify') {
+              // Navigating to First Otp Page
+            this.router.navigate(['/kyc/otp']);
+            } if (_userMode === 'AccountDetails') {
+              // Navigating to Set Transaction Pin Page
+            this.router.navigate(['vendor-bank']);
+            } if (_userMode === 'Confirm') {
+              // Navigating to Cooperator Dashboard
+            this.router.navigate(['vendor/my-transaction-history']);
+            }
         }
       } else {
       }
